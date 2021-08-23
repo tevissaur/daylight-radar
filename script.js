932f0b2b56ae5bdf62897e28fc66192196751846
Tevissaur <berserkboar@protonmail.com>
2021-08-23 17:00:18 -0500

    starting on localstorage

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 
 // Updates time every second
-let currentDayDisplay = $('#currentDay').text(moment().format('LLLL'))
+let currentDayDisplay = $('#currentDay')
+currentDayDisplay.text(moment().format('LLLL'))
 let currentHour = moment().hour()
 let timeBlocks = []
 
 class hourBlock {
-    constructor(time, task) {
+    constructor(time, taskEl, buttonEl) {
         this.time = time
-        this.task = task
+        this.taskEl = taskEl
+        this.buttonEl = buttonEl
+    }
+    loadEvents() {
+        localStorage.setItem('test', 'value')
+        this.taskEl.val(localStorage.getItem('test'))
+    }
+    saveButton() {
+        this.buttonEl.on('click', function (e) {
+            console.log(e.target)
+            let target = e.target
+            let hourBlock = $(e.target).closest('div.row').find('.hour-num').text()
+            let task = $(e.target).closest('div.row').find('textarea').val()
+        })
     }
 }
 
-// $('.saveBtn').click(function (e) {
-//     // e.stopPropagation()
-//     // let target = e.target
-//     // let hourBlock = $(e.target).closest('div.row').find('.hour-num').text()
-//     // let task = $(e.target).closest('div.row').find('textarea').val()
-//     // localStorage.setItem(hourBlock + ' hour block', task)
-// })
 
 setInterval(function () {
-    $('#currentDay').text(moment().format('LLLL'))
+    currentDayDisplay.text(moment().format('LLLL'))
     currentHour = moment().hour()
 }, 1000)
 
 
 $('.time-block').each(function (i) {
-    timeBlocks.push(new hourBlock((i+9), $(this).find('textarea').val()))
+
+    timeBlocks.push(new hourBlock((i + 9), $(this).find('textarea'), $(this).find('.saveBtn')))
+    timeBlocks[i].loadEvents()
+    timeBlocks[i].saveButton()
     if (moment(currentHour).isSame(timeBlocks[i].time)) {
-        
-        console.log($(this).find('textarea'), currentHour)
+
         $(this).find('textarea').toggleClass('past present')
-        
+
     } else if (moment(currentHour).isBefore(timeBlocks[i].time)) {
-        
+
         $(this).find('textarea').toggleClass('past future')
-        
+
     }
-    
+
 })
 console.log(timeBlocks)

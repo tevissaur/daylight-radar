1010322715f6a8f1deffc6b2572616639e34c879
Tevissaur <berserkboar@protonmail.com>
2021-08-23 16:31:44 -0500

    jquery is awesome

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 // Updates time every second
 let currentDayDisplay = $('#currentDay').text(moment().format('LLLL'))
 let currentHour = moment().hour()
-let time9am = moment().hour(9)
-let time10am = moment().hour(10)
-let time11am = moment().hour(11)
-let time12pm = moment().hour(12)
-let time1pm = moment().hour(13)
-let time2pm = moment().hour(14)
-let time3pm = moment().hour(15)
-let time4pm = moment().hour(16)
-let time5pm = moment().hour(17)
 let timeBlocks = []
 
 class hourBlock {
@@ -19,11 +10,6 @@ class hourBlock {
         this.task = task
     }
 }
-
-
-
-
-
 
 // $('.saveBtn').click(function (e) {
 //     // e.stopPropagation()
@@ -39,14 +25,18 @@ setInterval(function () {
 }, 1000)
 
 
-
 $('.time-block').each(function (i) {
-    console.log(i + 9)
     timeBlocks.push(new hourBlock((i+9), $(this).find('textarea').val()))
-    console.log(timeBlocks)
-    if (moment(currentHour).isSame(timeBlocks[i])) {
+    if (moment(currentHour).isSame(timeBlocks[i].time)) {
+        
         console.log($(this).find('textarea'), currentHour)
-        $(this).find('textarea').toggleClass('past present future')
-    } 
-
+        $(this).find('textarea').toggleClass('past present')
+        
+    } else if (moment(currentHour).isBefore(timeBlocks[i].time)) {
+        
+        $(this).find('textarea').toggleClass('past future')
+        
+    }
+    
 })
+console.log(timeBlocks)
